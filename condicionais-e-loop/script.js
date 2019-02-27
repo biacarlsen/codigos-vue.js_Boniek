9529e95cc9b7348d9f2fec4023928c25ae4275e2
Bianca Carlsen <bia.carlsen@gmail.com>
2019-02-27 16:02:48 -0300

    Criando condicionais e loops

---
+++ condicionais-e-loop/script.js
@@ -0,0 +1,24 @@
+// let placar = new Vue({
+// 	el:'#placar',
+// 	data:{
+// 		n = 0,
+// 	}
+// })
+
+let lista = new Vue({
+	el:'#lista',
+	data:{
+		nomes: [
+			{nome: "Bianca", sobrenome: "Carlsen", idade: 22},
+			{nome: "Maria", sobrenome: "Souza", idade: 23},
+			{nome: "Marina", sobrenome: "Duarte", idade: 20},
+			{nome: "Ila", sobrenome: "Flor", idade: 2},
+			{nome: "Luna", sobrenome: "Carlsen", idade: 5},
+		]
+	}
+});
+
+
+
+
+
